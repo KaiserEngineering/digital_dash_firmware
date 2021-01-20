60a4b97c504c73ca6be8e0ac943f7d1fdd4a84ac
Matt Kaiser <matt@kaiserengineering.io>
2021-01-19 21:26:27 -0500

    Remove spoof output files

--- Spoof/lib/lib_unit_conversion/src/lib_unit_conversion.d
+++
@@ -1,5 +0,0 @@
-lib/lib_unit_conversion/src/lib_unit_conversion.o: \
- ../lib/lib_unit_conversion/src/lib_unit_conversion.c \
- C:/Users/Matth/Documents/git/GitHub/digital-dash-firmware/lib/lib_unit_conversion/inc/lib_unit_conversion.h
-
-C:/Users/Matth/Documents/git/GitHub/digital-dash-firmware/lib/lib_unit_conversion/inc/lib_unit_conversion.h:
